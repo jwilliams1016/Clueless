bd25cb8d066bb39d5d3189e6a4de1a83461ec239
jwilliams1016 <jamiew1@umbc.edu>
2014-12-14 12:24:29 -0500

    Update BillardRoom.java

--- ClueLess/src/main/java/com/clueless/BillardRoom.java
+++ ClueLess/src/main/java/com/clueless/BillardRoom.java
@@ -1,13 +1,31 @@
 package com.clueless;
 
 public class BillardRoom extends Location {
-		public BillardRoom (String name, String codename) {
-			super(name, codename);
+		public BillardRoom (String name) {
+			super(name);
 		}//end constructor
 		public void setMoveOptions(Location option1, Location option2, Location option3, Location option4) {
-			adjacent[0] = option1;
-			adjacent[1] = option2;
-			adjacent[2] = option3;
-			adjacent[3] = option4;
+			adjacent[0] = null;
+			adjacent[1] = null;
+			adjacent[2] = null;
+			adjacent[3] = null;
+			int count = 0;
+			if (option1.isOccupied() == false){
+				adjacent[count] = option1;
+				count++;
+			}//end if
+			if (option2.isOccupied() == false){
+				adjacent[count] = option2;
+				count++;
+			}//end if
+			if (option3.isOccupied() == false){
+				adjacent[count] = option3;
+				count++;
+			}//end if
+			if (option4.isOccupied() == false){
+				adjacent[count] = option4;
+				count++;
+			}//end if
 		}//end setToMoveOptions()
 }//end BillardRoom class
+lass
